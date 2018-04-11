b6763e336de76909a6662979167ac3d0d6a3205b
keidaariffin <keidaariffin97@gmail.com>
2018-04-11 17:25:54 +0800

    11/4/2018

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,34 +1,7 @@
-<!doctype html>
+
 @extends('layouts.app')
-<html lang="{{ app()->getLocale() }}">
-      
 
-    <head>
-
-
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-        
-
-        <!-- Fonts -->
-        
-
-          <!-- Styles -->
-         <style>
-      
-        </style> 
-    </head>
-    <body>
-       
+  
            {{--   @if (Route::has('login')) 
                 <div class="top-right links">
                     @auth
@@ -351,4 +324,4 @@
                 </footer>
              
     </body>
-</html>
+
